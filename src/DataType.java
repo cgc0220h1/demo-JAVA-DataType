86941301223d20133f5e9babe7e173062d27ce5d
vanduc2514 <vanduc2514@gmail.com>
2020-03-18 23:18:19 +0700

    First commit

---
+++ src/DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i + " có kiểu dữ liệu là int.");
+        float f = 20.5f;
+        System.out.println(f + "f có kiểu dữ lệu là float.");
+        double d = 20.5;
+        System.out.println(d + " có kiểu dữ liệu là double.");
+        boolean b = true;
+        System.out.println(b + " có kiểu dữ liệu là boolean.");
+        char c = 'a';
+        System.out.println(c + " có kiểu dữ liệu là char");
+        String s = "Hà Nội";
+        System.out.println(s + " có kiểu dữ liệu là string.");
+    }
+}
